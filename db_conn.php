fa89bc93c2ac96e38be543676488838c91915e0d
VIRTUE AINA <115047899+virtue-aina@users.noreply.github.com>
2023-01-16 01:09:39 +0000

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,8 +1,8 @@
 <?php
 define ('DB_HOST', 'localhost');
-define ('DB_USER' , 'unn_w22016723');
-define ('DB_PASSWORD' ,'@Kinyele3889');
-define ('DB_NAME','unn_w22016723');
+define ('DB_USER' , '/////////');
+define ('DB_PASSWORD' ,'////////');
+define ('DB_NAME','////////');
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
